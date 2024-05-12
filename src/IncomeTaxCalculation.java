2eac95e933073fce66171a5f3ddd1ba0c0fe926f
Kamal <ka627752@gmail.com>
2024-05-12 11:23:47 +0600

    first

---
+++ src/IncomeTaxCalculation.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class IncomeTaxCalculation {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Please enter your income: ");
+        double income = in.nextDouble();
+        System.out.println("Are you married? (Y/N) ");
+        String input = in.next();
+        int status;
+        if(input.equals("Y")){
+            status = TaxReturn.MARRIED;
+        }else{
+            status = TaxReturn.SINGLE;
+        }
+        TaxReturn aTaxReturn = new TaxReturn(income, status);
+        System.out.println("Tax:" + aTaxReturn.getTax());
+    }
+}
---
+++ src/TaxReturn.java
@@ -0,0 +1,39 @@
+public class TaxReturn {
+    public static final int SINGLE = 1;
+    public static final int MARRIED = 1;
+
+    private static final double RATE1 = 0.10;
+    private static final double RATE2 = 0.25;
+    private static final double RATE_SINGLE_LIMIT = 32000;
+    private static final double RATE_MARRIED_LIMIT = 64000;
+    private double income;
+    private int status;
+
+    public TaxReturn(double anIncome, int aStatus) {
+        income = anIncome;
+        status = aStatus;
+    }
+    public double getTax(){
+        double tax1 = 0;
+        double tax2 = 0;
+        if(status == SINGLE){
+            if(income<=RATE_SINGLE_LIMIT){
+                tax1 = RATE1 * income;
+            }
+            else{
+                tax1 = RATE1 * RATE_SINGLE_LIMIT;
+                tax2 = RATE2 * (income - RATE_SINGLE_LIMIT);
+            }
+        }
+        else{
+            if(income<=RATE_MARRIED_LIMIT){
+                tax1 = RATE1 * income;
+            }
+            else{
+                tax1 = RATE1 * RATE_MARRIED_LIMIT;
+                tax2 = RATE2 * (income - RATE_MARRIED_LIMIT);
+            }
+        }
+        return tax1 + tax2;
+    }
+}
